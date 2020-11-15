19c08374c6cf5a092612c94f0595f924a9b316fb
noobie-developer <abhi.xavier2000@gmail.com>
2020-11-15 13:19:27 +0530

    Adding auth directory

---
+++ src/app.ts
@@ -0,0 +1,33 @@
+import express from 'express';
+import 'express-async-errors';
+import { json } from 'body-parser';
+import cookieSession from 'cookie-session';
+
+import { currentUserRouter } from './routes/current-user';
+import { signinRouter } from './routes/signin';
+import { signoutRouter } from './routes/signout';
+import { signupRouter } from './routes/signup';
+import { errorHandler, NotFoundError } from '@apgittix/common';
+
+const app = express();
+app.set('trust proxy', true);
+app.use(json());
+app.use(
+  cookieSession({
+    signed: false,
+    secure: process.env.NODE_ENV !== 'test' //allow cookies over http if in a test environment
+  })
+);
+
+app.use(currentUserRouter);
+app.use(signinRouter);
+app.use(signupRouter);
+app.use(signoutRouter);
+
+app.get('*', async(req, res) => {
+  throw new NotFoundError();
+})
+
+app.use(errorHandler);
+
+export { app };
